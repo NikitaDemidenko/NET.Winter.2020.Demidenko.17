de3988229aadfa14ccb17a77be7823600fe99875
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-02-25 19:16:22 +0300

    feat: add implementation for Enumerable extension task.

---
+++ ConsoleTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using EnumerableExtensionTask;
+
+namespace _123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var stringList = new List<string> { "a", "ab", "abc", "abcd", "abcde" };
+            var intTest = intList.Where(x => x > 5);
+            foreach (var item in intTest)
+            {
+                Console.WriteLine(item);
+            }
+
+            var stringTest = stringList.Where(str => str.Length > 2);
+            foreach (var item in stringTest)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine(intList.Count(x => x > 3 && x < 8));
+
+            Console.WriteLine();
+            Console.WriteLine(stringList.Count());
+            Console.WriteLine();
+            var test = intList.Select(x => x * 12.5);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            intTest = (intList as IEnumerable<int>).Reverse();
+            foreach (var item in intTest)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            stringTest = stringList.Select(str => str + "TEST");
+            foreach (var item in stringTest)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ EnumerableExtensionTask/Enumerable.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EnumerableExtensionTask
+{
+    public static class Enumerable
+    {
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null.");
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} cannot be null.");
+            }
+
+            return WhereIterator(source, predicate);
+
+            static IEnumerable<TSource> WhereIterator(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+            {
+                foreach (var item in source)
+                {
+                    if (predicate(item))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> OrderBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> key)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return OrderByIterator(source, key);
+
+            static IEnumerable<TSource> OrderByIterator(IEnumerable<TSource> source, Func<TSource, TKey> key)
+            {
+                var array = source.ToArray();
+                var keysArray = new TKey[array.Length];
+                int i = 0;
+                foreach (var item in array)
+                {
+                    keysArray[i++] = key(item);
+                }
+
+                Array.Sort(keysArray, array);
+                foreach (var item in array)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> OrderBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> key, IComparer<TKey> comparer)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (comparer is null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            return OrderByIterator(source, key, comparer);
+
+            static IEnumerable<TSource> OrderByIterator(IEnumerable<TSource> source, Func<TSource, TKey> key, IComparer<TKey> comparer)
+            {
+                var array = source.ToArray();
+                var keysArray = new TKey[array.Length];
+                int i = 0;
+                foreach (var item in array)
+                {
+                    keysArray[i++] = key(item);
+                }
+
+                Array.Sort(keysArray, array, comparer);
+                foreach (var item in array)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> OrderByDescending<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> key)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return OrderBy(source, key).Reverse();
+        }
+
+        public static IEnumerable<TSource> OrderByDescending<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> key, IComparer<TKey> comparer)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (comparer is null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            return OrderBy(source, key, comparer);
+        }
+
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            return AllIterator(source, predicate);
+
+            static bool AllIterator(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+            {
+                foreach (var item in source)
+                {
+                    if (!predicate(item))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> transformer)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null.");
+            }
+
+            if (transformer is null)
+            {
+                throw new ArgumentNullException($"{nameof(transformer)} cannot be null.");
+            }
+
+            return SelectIterator(source, transformer);
+
+            static IEnumerable<TResult> SelectIterator(IEnumerable<TSource> source, Func<TSource, TResult> transformer)
+            {
+                foreach (var item in source)
+                {
+                    yield return transformer(item);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> Cast<TResult>(this IEnumerable source)
+        {
+            if (source is IEnumerable<TResult> typedSource)
+            {
+                return typedSource;
+            }
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return CastIterator(source);
+
+            static IEnumerable<TResult> CastIterator(IEnumerable source)
+            {
+                foreach (object? obj in source)
+                {
+                    yield return (TResult)obj!;
+                }
+            }
+        }
+
+        public static IEnumerable<int> Range<TSource>(int start, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentException($"{nameof(count)} cannot be negative.");
+            }
+
+            long max = (long)(start + count - 1);
+
+            if (max > int.MaxValue)
+            {
+                throw new ArgumentException($"Max value of sequence is out of range.");
+            }
+
+            for (int i = start; i <= count + start; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public static int Count<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            return CountIterator(source, predicate);
+
+            static int CountIterator(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+            {
+                int count = 0;
+                foreach (var item in source)
+                {
+                    if (predicate(item))
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public static int Count<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return CountIterator(source);
+
+            static int CountIterator(IEnumerable<TSource> source)
+            {
+                int count = 0;
+                foreach (var item in source)
+                {
+                    count++;
+                }
+
+                return count;
+            }
+        }
+
+        public static IEnumerable<T> Reverse<T>(this IEnumerable<T> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return ReverseIterator(source);
+
+            static IEnumerable<T> ReverseIterator(IEnumerable<T> source)
+            {
+                var array = source.ToArray();
+
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    yield return array[i];
+                }
+            }
+        }
+
+        public static TSource[] ToArray<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return ToArrayIterator(source);
+
+            static TSource[] ToArrayIterator(IEnumerable<TSource> source)
+            {
+                var array = new TSource[source.Count()];
+                int i = 0;
+                foreach (var item in source)
+                {
+                    array[i++] = item;
+                }
+
+                return array;
+            }
+        }
+    }
+}
